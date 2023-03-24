acf7d30d97ad71c8a158c220fea33a387315dc69
Freedom Penguin <papazogloy59@gmail.com>
2023-03-24 15:02:55 +0200

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+## How to use 
+```bash
+$ bash remove-duplicates.sh
+```
+
+## LICENSE
+The script is signed under **DO THE FUCK YOU WANT LICENSE**
--- remove-duplicates.sh
+++ remove-duplicates.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-
-# remove duplicate files script
-
 root_dir=$1
-
 for file in "$root_dir"/*; do
-	# check if the file is a regular file
 	if [ -f "$file" ]; then
-		# get the file name
 		file_name=$(basename "$file")
-		# find all files with the same name in the subdirectories
 		duplicate_files=($(find "$root_dir" -name "$file_name" -not -path "$file"))
-		# check if there are duplicates
 		if [ ${#duplicate_files[@]} -gt 0 ]; then
-			# remove the file in the parent director
 			echo "removing file $file"
 			rm "$file"
 		fi
